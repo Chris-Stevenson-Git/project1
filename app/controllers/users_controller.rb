8eb80d8b9a720f47f3f3675b563562cad1c380cb
Chris-Stevenson-Git <christopher-stevenson@outlook.com>
2020-08-07 21:58:46 +1000

    Filled out functionality for sessions and new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,21 @@
 class UsersController < ApplicationController
   def new
+    #new user object stored in ruby memory
+    @user = User.new
   end
 
   def create
+    @user = User.create user_params
+
+    #if the creation was successful
+    if @user.presisted?
+      session[:user_id] = @user.id
+      redirect_to home_path
+    else
+      #Else soft refresh of the page
+      render :new
+    end
+
   end
 
   def index
@@ -19,4 +32,13 @@ class UsersController < ApplicationController
 
   def destroy
   end
-end
+
+  #private methods below
+  private
+
+  def user_params
+    #Submitted params MUST be the below
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end #User controller
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+#root routes
+root to: 'pages#welcome'
+get '/home' => 'pages#home'
+
+#session routes
+get '/login' => 'session#new' #login form
+post '/login' => 'session#create' #form submit and authenticate
+delete '/login' => 'session#destroy' #logout
+
 #CRUD
 resources :users
 resources :recipes
