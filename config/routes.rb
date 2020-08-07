32c101b95658551ec73c3df597ac70f45ff73bac
Chris-Stevenson-Git <christopher-stevenson@outlook.com>
2020-08-07 19:46:14 +1000

    Added user model, controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#CRUD for users
+resources :users
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding Database..."
+puts ''
+puts 'Creating User Accounts...'
+
+User.destroy_all
+
+u1 = User.create! email: 'chris@ga.com', password: 'chicken', name: 'Chris', image: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+u2 = User.create! email: 'luke@ga.com', password: 'chicken', name: 'Luke', image: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+u3 = User.create! email: 'zara@ga.com', password: 'chicken', name: 'Zara', image: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+
+puts "Created #{User.count} users: "
+puts User.pluck(:name).join(', ')
